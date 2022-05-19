35591b43c1ad9f863c957ead7b2126b626e98147
Falendrac <leger_franck@hotmail.fr>
2022-05-19 11:37:10 +0200

    Add getter method in file_storage.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,19 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """
+        Get an instance of a specific class (cls) and id and
+        return the instance
+        """
+        if cls is None or id is None:
+            return (None)
+
+        cls_catch = None
+        cls_all = self.all(cls)
+        for key, value in cls_all.items():
+            if value.id == id:
+                cls_catch = value
+
+        return (cls_catch)
