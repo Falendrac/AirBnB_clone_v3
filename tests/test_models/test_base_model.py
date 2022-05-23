07c2bc992506309536eb11bd19791e631fe5602e
Falendrac <leger_franck@hotmail.fr>
2022-05-23 14:26:04 +0200

    Redo correctly import in test_base_model.

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,11 +2,10 @@
 """Test BaseModel for expected behavior and documentation"""
 from datetime import datetime
 import inspect
-import unittest
 import models
-import pycodestyle
+import pep8 as pycodestyle
 import time
-import pep8
+import unittest
 from unittest import mock
 BaseModel = models.base_model.BaseModel
 module_doc = models.base_model.__doc__
